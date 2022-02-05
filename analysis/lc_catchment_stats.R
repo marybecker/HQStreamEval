06881247bf013239ae51c54a484a76cdd2607ae3
marybecker <mary.becker@ct.gov>
2022-02-05 18:31:25 -0500

    start lc tool dev

--- analysis/lc_catchment_stats.R
+++ analysis/lc_catchment_stats.R
@@ -14,6 +14,8 @@ setwd("D:/HQEval_spatial_data/")
 r_path <- "raster/"
 sf_path <- "vector/"
 
+### functions ##################################################################
+
 # functions to cycle through lc years, reclassify for a particular land cover
 # and calc zonal stats for catchment polys(i.e. pct lc by catchment)
 
@@ -22,51 +24,47 @@ sf_path <- "vector/"
 # l_type = type of land cover being reclassified
 reclass_lc <- function(r_file,l_spec,l_type){
   for (i in 1:(length(r_file))){
-    lc_yr <- str_sub(r_file[i],-8,-5)
-    lc <- rast(paste0(r_path,r_file[i]))
-    rc <- classify(lc, mat,include.lowest=FALSE)
+    lc_yr <- str_sub(r_file[i],-8,-5) # get year
+    lc <- rast(paste0(r_path,r_file[i])) # read in raster file
+    rc <- classify(lc, mat,include.lowest=FALSE) # reclassify
     writeRaster(rc,paste0("raster/ag_",lc_yr,".tif"),overwrite=TRUE)
   }
 }
 
 extract_lc <- function(r_file,poly,l_type){
   for (i in 1:(length(r_file))){
-    lc_yr <- str_sub(r_file[i],-8,-5)
-    lc <- rast(paste0(r_path,r_file[i]))
+    lc_yr <- str_sub(r_file[i],-8,-5) #get year
+    lc <- rast(paste0(r_path,r_file[i])) #read in raster file
+    # calc total cnt of pixels
     l_ex <- extract(lc, poly, fun=function(x,...)length(na.omit(x)),df=TRUE)
+    # calc sun of (0,1) pixels
     ex <- extract(lc, poly, fun=sum, na.rm=TRUE, df=TRUE)
     df <- data.frame(poly$HydroID,l_ex[,2],ex[,2])
     df[,3] <- df[,3]-1
-    df$pct <- df[,3]/df[,2]
+    df$pct <- df[,3]/df[,2] # calc proportion land cover
     colnames(df) <- c("HydroID","cnt",paste0(l_type,"_",lc_yr,"_sum"),paste0(l_type,"_",lc_yr,"_pct"))
     write.csv(df,paste0(l_type,"_",lc_yr,".csv"),row.names=FALSE)
   }
 }
 
+
+### reclassify #################################################################
+
 # list files
 grids    <- list.files(r_path, pattern = "*.tif$")
 lc_grids <- list.files(r_path, pattern = "landcover_*")
-ag_grids <- list.files(r_path, pattern = "ag_*")
 
-### reclassify #################################################################
 c_spec <- cbind(from = c(-Inf, 3, 4), to = c(3, 4, Inf), becomes = c(0, 1, 0))
-c_type <- "ag"
 
-reclass_lc(lc_grids,c_spec,c_type)
-
-ag <- rast(paste0(r_path,'ag_1985.tif'))
+reclass_lc(lc_grids,c_spec,"ag")
 
 ### extract by poly ############################################################
 
-#read-in the polygon shapefile
-poly <- vect(paste0(sf_path,"Catchments01_CT.shp"))
+# get only specified landcover grids
+ag_grids <- list.files(r_path, pattern = "ag_*")
 
-#import raster file
-# s <- rast(paste0(rf_path, grids))
-# l <- extract(s, poly, fun=function(x,...)length(na.omit(x)),df=TRUE)
-# 
-# l <- data.frame(poly$HydroID,l[,2])
-# colnames[l] <- c("HydroID","Cnt")
+#read-in the polygon shapefile of river catchments
+poly <- vect(paste0(sf_path,"Catchments01_CT.shp"))
 
 extract_lc(ag_grids,poly,"ag")
 
