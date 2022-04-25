b7e002fd91d5068a90eb21ced7257882de5abe29
Mary Becker <mary.becker@ct.gov>
2022-04-25 09:24:06 -0400

    map template

---
+++ js/main.js
@@ -0,0 +1,30 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoibWFyeS1iZWNrZXIiLCJhIjoiY2p3bTg0bDlqMDFkeTQzcDkxdjQ2Zm8yMSJ9._7mX0iT7OpPFGddTDO5XzQ';
+
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: {
+        'version': 8,
+        'sources': {
+            'raster-tiles': {
+                'type': 'raster',
+                'tiles': [
+                    'https://basemap.nationalmap.gov/arcgis/rest/services/USGSHydroCached/MapServer/tile/{z}/{y}/{x}'
+                ],
+                'tileSize': 256,
+                'attribution':
+                    'USGS The National Map: National Hydrography Dataset'
+            }
+        },
+        'layers': [
+            {
+                'id': 'simple-tiles',
+                'type': 'raster',
+                'source': 'raster-tiles',
+                'minzoom': 0,
+                'maxzoom': 17
+            }
+        ]
+    },
+    center: [-72.65, 41.55], // starting position
+    zoom: 8.5 // starting zoom
+});
